358d0bc9b3f6579da1502efcd7df84cab68b143d
Nicholas Ferradino <nferradino@gmail.com>
2022-11-18 13:52:36 -0500

    final commit

---
+++ build/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/dictionary.cpp.o"
+  "CMakeFiles/calculator.dir/dictionary.cpp.o.d"
+  "CMakeFiles/calculator.dir/fraction.cpp.o"
+  "CMakeFiles/calculator.dir/fraction.cpp.o.d"
+  "CMakeFiles/calculator.dir/main.cpp.o"
+  "CMakeFiles/calculator.dir/main.cpp.o.d"
+  "calculator"
+  "calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
